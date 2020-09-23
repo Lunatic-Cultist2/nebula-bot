35a9837d765ecd0ee58f8966e0ad5c3e5b47a682
Lunatic Cultist <haddi.tariqch1@gmail.com>
2020-09-23 15:31:52 +1000

    Automatic Status Change

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,4 +1,5 @@
 const Event = require('../Structures/Event');
+const { version } = require('../../package.json');
 
 module.exports = class extends Event {
 
@@ -14,5 +15,20 @@ module.exports = class extends Event {
 			`Loaded: ${this.client.commands.size} Commands!`,
 			`Loaded: ${this.client.events.size} Events!`
 		].join('\n'));
+
+		this.client.user.setStatus('idle');
+
+		const activities = [
+			`${this.client.guilds.cache.size} Server(s)!`,
+			`${this.client.channels.cache.size} Channel(s)!`,
+			`${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} User(s)!`,
+			`${version}`,
+			`Lunatic Cultist さん#6486`
+		];
+
+		let i = 0;
+		setInterval(() => this.client.user.setActivity(`${this.client.prefix}help | ${activities[i++ % activities.length]}`, { type: 'PLAYING' }), 15000);
+
+		// this.client.user.setActivity('discord.js', { type: 'PLAYING'});
 	}
 };
